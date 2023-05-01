00233bff8808fe2f5d01066b15cd8556c882b7ef
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-05-02 00:00:27 +0600

    first commit

---
+++ src/components/SingUp/SingUp.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './SingUp.css'
+import { Link } from 'react-router-dom';
+
+const SingUp = () => {
+
+    const [error, setError]= useState('');
+
+    const handleSingUp = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const pass =form.password.value;
+        const confirmPass = form.confirm.value;
+        console.log(email, pass, confirmPass)
+
+        if(pass !== confirmPass){
+            setError('Your Pass Didnt match')
+            return;
+        }
+        else if(pass.length < 6){
+            setError('Please Give 6 digit password')
+            return;
+        }
+
+    }
+    return (
+        <div>
+            <div className='form-container'>
+            <h2 className='form-title'>SingUp</h2>
+            <form onSubmit={handleSingUp}>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm">Password Confirm</label>
+                    <input type="password" name="confirm" id="" required/>
+                </div>
+                <input className='btn-submit' type="submit" value="SingUp" />
+                <p className='text-error'>{error}</p>
+            </form>
+            <p><small>Allready have an account?<Link to='/login'>Login</Link></small></p>
+        </div>
+        </div>
+    );
+};
+
+export default SingUp;
